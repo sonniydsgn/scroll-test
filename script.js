0ea17528bd94da644a8f9aeb7ce1255ff4dbf4d4
Alexey Kalinin <72299738+sonniydsgn@users.noreply.github.com>
2023-06-21 15:17:02 +0500

    обнова

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log(1);
+// gsap.timeline({
+//   scrollTrigger: {
+//     trigger: ".scroll-container",
+//     start: "center center",
+//     end: "bottom top",
+//     scrub: true,
+//     pin: true
+//   }
+// })
+//   .from(".img__top",  { y: -80 })
+//   .from(".img__middle", { y: 40 })
+//   .from(".img__bottom", { y: 120 })

e7579370afdaaa06066a66eec7dc66c3d34df414
Alexey Kalinin <72299738+sonniydsgn@users.noreply.github.com>
2023-06-25 01:58:15 +0500

    улучшения анимации

--- script.js
+++ script.js
@@ -1,67 +1,63 @@
-if ( screen.width >= 800 ) {
-  gsap.timeline({
-    scrollTrigger: {
-      trigger: ".scroll-wrapper",
-      start: "top top",
-      end: "+=4000",
-      scrub: true,
-      pin: true,
+const cards = gsap.utils.toArray(".card__info");
+
+// function IsActive (i) {
+//   document.querySelector(`#section-${i}`).classList.add('active')
+// }
+
+cards.forEach((card, i) => {
+  ScrollTrigger.create({
+    trigger: card,
+    start: "top center",
+    end: "bottom bottom",
+    markers: true, 
+    onToggle: self => {
+      if (self.isActive && i === 0) {
+        gsap.to(".img__middle", {opacity: 0, display: 'none'})
+        gsap.to(".img__bottom", {opacity: 1, display: 'block'})
+      } else if (self.isActive && i === 1) {
+        gsap.to(".img__top", {opacity: 0, display: 'none'})
+        
+        gsap.to(".img__bottom", {opacity: 0, display: 'none'})
+        gsap.to(".img__middle", {opacity: 1, display: 'block'})
+      } else if (self.isActive && i === 2) {
+        gsap.to(".img__top", {y: 0})
+        gsap.to(".img__middle", {y: 0})
+        gsap.to(".img__bottom", {y: 0})
+        gsap.to(".img__bottom", {opacity: 0, display: 'none'})
+        gsap.to(".img__middle", {opacity: 0, display: 'none'})
+
+        gsap.to(".img__middle", {opacity: 0, display: 'none'})
+        gsap.to(".img__top", {opacity: 1, display: 'block'})
+      } else if (self.isActive && i === 3) {
+        gsap.to(".img__top", {y: -180})
+        gsap.to(".img__middle", {y: -40})
+        gsap.to(".img__bottom", {y: 130})
+
+        gsap.to(".img__top", {opacity: 1, display: 'block'})
+        gsap.to(".img__middle", {opacity: 1, display: 'block'})
+        gsap.to(".img__middle--bottom", {opacity: 1})
+        gsap.to(".img__bottom", {opacity: 1, display: 'block'})
+      }
     }
-  })
-  
-    // имплант -> абатмент
-    .to(".card", {y: "-100%", ease: "power1.in"}, 1)
-    .to(".img__bottom", {opacity: 0, display: 'none', ease: "power1.in"}, 1)
-    .to(".img__middle", {opacity: 1, display: 'block', ease: "power1.in"}, 1)
-  
-    // абатмент -> коронка
-    .to(".card", {y: "-200%", ease: "power1.in"}, 3)
-    .to(".img__middle", {opacity: 0, display: 'none', ease: "power1.in"}, 3)
-    .to(".img__top", {opacity: 1, display: 'block', ease: "power1.in"}, 3)
-  
-    // все вместе
-    .to(".card", {y: "-300%", ease: "power1.in"}, 5)
-    .to(".img__top", {y: -180, ease: "power1.in"}, 5)
-    .to(".img__middle", {y: -40, duration: 0.01}, 5)
-    .to(".img__bottom", {y: 130, duration: 0.01}, 5)
-    .to(".img__bottom", {opacity: 1, display: 'block', ease: "power1.in"}, 5)
-    .to(".img__middle", {opacity: 1, display: 'block', ease: "power1.in"}, 5)
-  
-    // соединение
-    .to(".img__top", { y: -90 }, 7)
-    .to(".img__middle--bottom", { opacity: 0 }, 7)
-    .to(".img__bottom",  { y: 70 }, 7)
-} else {
-  gsap.timeline({
-    scrollTrigger: {
-      trigger: ".scroll-wrapper",
-      start: "top top",
-      end: "+=4000",
-      scrub: true,
-      pin: true,
+  });
+})
+
+ScrollTrigger.create({
+  trigger: '.scroll-wrapper',
+  start: "bottom bottom",
+  end: "+=1000",
+  pin: true,
+  scrub: true,
+  onToggle: self => {
+    if (self.isActive) {
+      gsap.to(".img__top", {opacity: 1, display: 'block'})
+      gsap.to(".img__middle", {opacity: 1, display: 'block'})
+      gsap.to(".img__middle--bottom", {opacity: 1})
+      gsap.to(".img__bottom", {opacity: 1, display: 'block'})
+
+      gsap.to(".img__top", { y: -90, delay: 0.2 })
+      gsap.to(".img__middle--bottom", { opacity: 0, delay: 0.2 })
+      gsap.to(".img__bottom",  { y: 70, delay: 0.2 })
     }
-  })
-  
-    // имплант -> абатмент
-    .to(".card", {y: "-100%", ease: "power1.in"}, 1)
-    .to(".img__bottom", {opacity: 0, display: 'none', ease: "power1.in"}, 1)
-    .to(".img__middle", {opacity: 1, display: 'block', ease: "power1.in"}, 1)
-  
-    // абатмент -> коронка
-    .to(".card", {y: "-200%", ease: "power1.in"}, 3)
-    .to(".img__middle", {opacity: 0, display: 'none', ease: "power1.in"}, 3)
-    .to(".img__top", {opacity: 1, display: 'block', ease: "power1.in"}, 3)
-  
-    // все вместе
-    .to(".card", {y: "-300%", ease: "power1.in"}, 5)
-    .to(".img__top", {y: -130, ease: "power1.in"}, 5)
-    .to(".img__middle", {y: -30, duration: 0.01}, 5)
-    .to(".img__bottom", {y: 90, duration: 0.01}, 5)
-    .to(".img__bottom", {opacity: 1, display: 'block', ease: "power1.in"}, 5)
-    .to(".img__middle", {opacity: 1, display: 'block', ease: "power1.in"}, 5)
-  
-    // соединение
-    .to(".img__top", { y: -65 }, 7)
-    .to(".img__middle--bottom", { opacity: 0 }, 7)
-    .to(".img__bottom",  { y: 40 }, 7)
-}
+  }
+})

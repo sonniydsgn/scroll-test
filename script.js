d5311490c3bde3eb74d51fb1a08ba057946d8e9e
Alexey Kalinin <72299738+sonniydsgn@users.noreply.github.com>
2023-06-25 02:19:37 +0500

    подтянул код

--- script.js
+++ script.js
@@ -1,63 +1,53 @@
 const cards = gsap.utils.toArray(".card__info");
 
-// function IsActive (i) {
-//   document.querySelector(`#section-${i}`).classList.add('active')
-// }
-
 cards.forEach((card, i) => {
   ScrollTrigger.create({
     trigger: card,
     start: "top center",
-    end: "bottom bottom",
-    markers: true, 
+    end: "bottom center",
     onToggle: self => {
       if (self.isActive && i === 0) {
-        gsap.to(".img__middle", {opacity: 0, display: 'none'})
-        gsap.to(".img__bottom", {opacity: 1, display: 'block'})
+        gsap.to(".img__middle", {opacity: 0, display: 'none', duration: 0.2})
+        gsap.to(".img__bottom", {opacity: 1, display: 'block', duration: 0.2})
       } else if (self.isActive && i === 1) {
-        gsap.to(".img__top", {opacity: 0, display: 'none'})
+        gsap.to(".img__top", {opacity: 0, display: 'none', duration: 0.2})
         
-        gsap.to(".img__bottom", {opacity: 0, display: 'none'})
-        gsap.to(".img__middle", {opacity: 1, display: 'block'})
+        gsap.to(".img__bottom", {opacity: 0, display: 'none', duration: 0.2})
+        gsap.to(".img__middle", {opacity: 1, display: 'block', duration: 0.2})
       } else if (self.isActive && i === 2) {
-        gsap.to(".img__top", {y: 0})
-        gsap.to(".img__middle", {y: 0})
-        gsap.to(".img__bottom", {y: 0})
-        gsap.to(".img__bottom", {opacity: 0, display: 'none'})
-        gsap.to(".img__middle", {opacity: 0, display: 'none'})
+        gsap.to(".img__top", {y: 0, duration: 0.2})
+        gsap.to(".img__middle", {y: 0, duration: 0.2})
+        gsap.to(".img__bottom", {y: 0, duration: 0.2})
+        gsap.to(".img__bottom", {opacity: 0, display: 'none', duration: 0.2})
+        gsap.to(".img__middle", {opacity: 0, display: 'none', duration: 0.2})
 
-        gsap.to(".img__middle", {opacity: 0, display: 'none'})
-        gsap.to(".img__top", {opacity: 1, display: 'block'})
+        gsap.to(".img__middle", {opacity: 0, display: 'none', duration: 0.2})
+        gsap.to(".img__top", {opacity: 1, display: 'block', duration: 0.2})
       } else if (self.isActive && i === 3) {
-        gsap.to(".img__top", {y: -180})
-        gsap.to(".img__middle", {y: -40})
-        gsap.to(".img__bottom", {y: 130})
+        gsap.to(".img__top", {y: -180, duration: 0.2})
+        gsap.to(".img__middle", {y: -40, duration: 0.2})
+        gsap.to(".img__bottom", {y: 130, duration: 0.2})
 
-        gsap.to(".img__top", {opacity: 1, display: 'block'})
-        gsap.to(".img__middle", {opacity: 1, display: 'block'})
-        gsap.to(".img__middle--bottom", {opacity: 1})
-        gsap.to(".img__bottom", {opacity: 1, display: 'block'})
+        gsap.to(".img__top", {opacity: 1, display: 'block', duration: 0.2})
+        gsap.to(".img__middle", {opacity: 1, display: 'block', duration: 0.2})
+        gsap.to(".img__middle--bottom", {opacity: 1, duration: 0.2})
+        gsap.to(".img__bottom", {opacity: 1, display: 'block', duration: 0.2})
       }
     }
   });
 })
 
-ScrollTrigger.create({
-  trigger: '.scroll-wrapper',
-  start: "bottom bottom",
-  end: "+=1000",
-  pin: true,
-  scrub: true,
-  onToggle: self => {
-    if (self.isActive) {
-      gsap.to(".img__top", {opacity: 1, display: 'block'})
-      gsap.to(".img__middle", {opacity: 1, display: 'block'})
-      gsap.to(".img__middle--bottom", {opacity: 1})
-      gsap.to(".img__bottom", {opacity: 1, display: 'block'})
-
-      gsap.to(".img__top", { y: -90, delay: 0.2 })
-      gsap.to(".img__middle--bottom", { opacity: 0, delay: 0.2 })
-      gsap.to(".img__bottom",  { y: 70, delay: 0.2 })
-    }
+gsap.timeline({
+  scrollTrigger: {
+    trigger: ".scroll-wrapper",
+    start: "bottom bottom",
+    end: "+=800",
+    scrub: true,
+    pin: true,
   }
 })
+
+  // имплант -> абатмент
+  .to(".img__top", { y: -90 }, 1)
+  .to(".img__middle--bottom", { opacity: 0 }, 1)
+  .to(".img__bottom",  { y: 70 }, 1)

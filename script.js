f5b28e7667d5d131dfbd89c677492a9b61bc46c0
Alexey Kalinin <72299738+sonniydsgn@users.noreply.github.com>
2023-06-25 02:27:10 +0500

    мобильная версия анимации

--- script.js
+++ script.js
@@ -24,9 +24,9 @@ cards.forEach((card, i) => {
         gsap.to(".img__middle", {opacity: 0, display: 'none', duration: 0.2})
         gsap.to(".img__top", {opacity: 1, display: 'block', duration: 0.2})
       } else if (self.isActive && i === 3) {
-        gsap.to(".img__top", {y: -180, duration: 0.2})
-        gsap.to(".img__middle", {y: -40, duration: 0.2})
-        gsap.to(".img__bottom", {y: 130, duration: 0.2})
+        gsap.to(".img__top", {y: "-150%", duration: 0.2})
+        gsap.to(".img__middle", {y: "-40%", duration: 0.2})
+        gsap.to(".img__bottom", {y: "50%", duration: 0.2})
 
         gsap.to(".img__top", {opacity: 1, display: 'block', duration: 0.2})
         gsap.to(".img__middle", {opacity: 1, display: 'block', duration: 0.2})
@@ -48,6 +48,6 @@ gsap.timeline({
 })
 
   // имплант -> абатмент
-  .to(".img__top", { y: -90 }, 1)
+  .to(".img__top", { y: "-80%" }, 1)
   .to(".img__middle--bottom", { opacity: 0 }, 1)
-  .to(".img__bottom",  { y: 70 }, 1)
+  .to(".img__bottom",  { y: "23%" }, 1)
